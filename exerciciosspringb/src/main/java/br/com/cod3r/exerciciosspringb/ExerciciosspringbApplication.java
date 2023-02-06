6be30df66c1faeea5f3e8db04f84f78c3c3ca6e0
Vinícius Monteiro <70413886+monteirote@users.noreply.github.com>
2023-02-06 17:01:32 -0300

    Add files via upload

---
+++ exerciciosspringb/src/main/java/br/com/cod3r/exerciciosspringb/ExerciciosspringbApplication.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.exerciciosspringb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosspringbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosspringbApplication.class, args);
+	}
+
+}
---
+++ exerciciosspringb/src/test/java/br/com/cod3r/exerciciosspringb/ExerciciosspringbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.exerciciosspringb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosspringbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
